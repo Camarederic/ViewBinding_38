53630550aaab800ec45f01b8ef33cde4007875f6
Camarederic <camarederic@mail.ru>
2022-11-04 22:29:54 +0300

    Create ViewBinding

--- app/src/main/java/com/example/viewbinding_38/MainActivity.kt
+++ app/src/main/java/com/example/viewbinding_38/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.viewbinding_38
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.viewbinding_38.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    val a = 150
+    val b = 250
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonAddition.setOnClickListener {
+            val result = a + b
+            binding.textView.text = "Результат сложения равен: $result"
+        }
+
+        binding.buttonSubtraction.setOnClickListener {
+            val result2 = a - b
+            binding.textView.text = "Результат вычитания равен: $result2"
+        }
+
+        binding.buttonMultiplication.setOnClickListener {
+            val result3 = a * b
+            binding.textView.text = "Результат умножения равен: $result3 "
+        }
     }
 }
